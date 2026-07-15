7edc30ea7664720dd9aa47ca17640c01504a5028
Dhruv Nimbalkar <119033607+DhruvNimbalkar04@users.noreply.github.com>
2026-07-15 23:18:42 +0530

    Added custom user model and updated settings

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    phone_number = PhoneNumberField(
+        unique=True,
+        blank=True,
+        null=True
+    )
+
+    is_verified = models.BooleanField(default = False)
+
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'search',
     'returns',
     'common',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
