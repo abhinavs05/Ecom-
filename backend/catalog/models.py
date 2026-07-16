d9a561a68a843aceae7e5882c89e5ccf3dc58b09
Dhruv Nimbalkar <119033607+DhruvNimbalkar04@users.noreply.github.com>
2026-07-16 09:48:04 +0530

    updates catalog

--- backend/catalog/models.py
+++ backend/catalog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    slug = models.SlugField(unique=True)
+
+    image = models.ImageField(
+        upload_to="categories/",
+        blank=True,
+        null=True
+    )
+
+    description = models.TextField(blank=True)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
+
+    name = models.CharField(max_length=200)
+
+    slug = models.SlugField(unique=True)
+
+    description = models.TextField()
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    stock = models.PositiveIntegerField()
+
+    image = models.ImageField(upload_to="products/")
+
+    available = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="images"
+    )
+
+    image = models.ImageField(upload_to="products/")
