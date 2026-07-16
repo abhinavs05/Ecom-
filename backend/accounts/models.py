2c9a375662baace21a6168779b1c4e5075906616
Dhruv Nimbalkar <119033607+DhruvNimbalkar04@users.noreply.github.com>
2026-07-16 09:18:41 +0530

    Refactor user, profile, and address models

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -11,10 +11,40 @@ class User(AbstractUser):
         null=True
     )
 
-    is_verified = models.BooleanField(default = False)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
 
+class Profile(models.Model):
+    user = models.OneToOneFiel(User, on_delete=models.CASCADE , related_name='profile')
+    dob = models.DateField()
+    
+    class Gender(models.TextChoices):
+        MALE = "Male", "Male"
+        FEMALE = "Female", "Female"
+
+    gender = models.CharField(
+        max_length=50,
+        choices=Gender.choices
+        )
+    
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE , related_name='addresses')
+
+    full_name = models.CharField(max_length=50,default=user.username)
+
+    phone = PhoneNumberField()
+
+    address_line_1 = models.CharField(max_length=255)
+
+    address_line_2 = models.CharField(max_length=50,blank=True)
+
+    city = models.CharField(max_length=100)
+
+    state = models.CharField(max_length=50)
+
+    postal_code = models.CharField(max_length=50)
+
+    country = models.CharField(max_length=50)
