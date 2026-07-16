981eadeb85b9e48511ab7cc8da6f599036a515ad
Dhruv Nimbalkar <119033607+DhruvNimbalkar04@users.noreply.github.com>
2026-07-16 10:15:18 +0530

    Added Review and Wishlist models

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from accounts.models import User
+from catalog.models import Product
 
-# Create your models here.
+class Review(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="reviews"
+    )
+
+    rating = models.PositiveSmallIntegerField()
+
+    comment = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "product")
--- backend/wishlist/models.py
+++ backend/wishlist/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from accounts.models import User
+from catalog.models import Product
 
-# Create your models here.
+class Wishlist(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="wishlist"
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "product")
