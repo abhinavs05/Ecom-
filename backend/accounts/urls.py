366cc1d3d48a7aa2bc729a77d0a81195b4d9128e
Dhruv Nimbalkar <119033607+DhruvNimbalkar04@users.noreply.github.com>
2026-07-17 22:09:11 +0530

    Added Register View in serialers and made urls

---
+++ backend/accounts/urls.py
@@ -0,0 +1,13 @@
+from .views import RegisterView
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path("register/", RegisterView.as_view()),
+    path("login/", TokenObtainPairView.as_view()),
+    path("refresh/", TokenRefreshView.as_view()),
+]
